62b2d0a4d974d0cb31e63b2606c6f0093f9e3b75
Carlos Costa <ca.costa@swordhealth.com>
2025-10-29 16:20:16 +0000

    Initial Commit

---
+++ Breeds/BreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreedsApp.swift
+//  Breeds
+//
+//  Created by Carlos Costa on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
